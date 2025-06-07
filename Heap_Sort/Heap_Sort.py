16e8957003ec9f4669a0a6b4999ae3d0e70cc0e2
Miguel Angel López <153057889+Pymike8@users.noreply.github.com>
2025-06-07 03:41:25 -0600

    Update Heap_Sort.py

--- Heap_Sort/Heap_Sort.py
+++ Heap_Sort/Heap_Sort.py
@@ -1,6 +1,3 @@
-# Explicación: Utiliza una estructura de datos en montículo (heap) para ordenar elementos. Primero construye un max-heap, luego extrae repetidamente el elemento máximo.
-# Complejidad: O(n log n) en todos los casos
-
 def heap_sort(arr):
     n = len(arr)
 
