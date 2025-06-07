05f8ebf46ee2d3daff90d86e499bba351d66a598
Miguel Angel López <153057889+Pymike8@users.noreply.github.com>
2025-06-07 03:41:05 -0600

    Update Bucket_Sort.py

--- Bucket_Sort/Bucket_Sort.py
+++ Bucket_Sort/Bucket_Sort.py
@@ -1,6 +1,3 @@
-# Explicación: Distribuye los elementos en varios cubos (buckets), luego ordena cada cubo individualmente (usando otro algoritmo o recursivamente) y finalmente concatena los resultados.
-# Complejidad: O(n + k) en el mejor caso, O(n²) en el peor caso
-
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
